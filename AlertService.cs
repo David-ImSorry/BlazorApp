cb80d6f128d5f29024e0594606643bba3797b644
David-ImSorry <jd.barbetti@gmail.com>
2024-04-07 19:52:23 -0500

    Home.Razor AlertService

---
+++ AlertService.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.JSInterop;
+
+namespace BlazorApp1
+{
+	public class AlertService : IAsyncDisposable
+	{
+		readonly Lazy<Task<IJSObjectReference>> ijsObjectReference;
+
+		public AlertService(IJSRuntime ijsRuntime)
+		{
+			this.ijsObjectReference = new Lazy<Task<IJSObjectReference>>(()=>
+			ijsRuntime.InvokeAsync<IJSObjectReference>("import", "./content/BlazorApp3/Page/Home.razor.js").AsTask());
+		}
+
+		public ValueTask DisposeAsync()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
